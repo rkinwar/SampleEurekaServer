ce9a9ee28bb054af9f2abec60ca31ae109a1b7ae
rkinwar <rkinwar@deloitte.ca>
2019-03-25 11:02:56 -0400

    Initial Commit - Eureka Server

---
+++ src/main/java/com/discovery/SampleEurekaServer/SampleEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.discovery.SampleEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleEurekaServerApplication.class, args);
+	}
+
+}
